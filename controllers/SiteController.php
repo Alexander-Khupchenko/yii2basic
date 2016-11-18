8cfe906fcd1459dd06531a125b6da40c9673b099
Alexander-Khupchenko <khupchenko86@gmail.com>
2016-11-18 14:53:26 +0200

    session, cookies

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -174,13 +174,20 @@ class SiteController extends Controller
         
         return $this->render('comments', [
             'comments' => $comments,
-            'pagination' => $pagination
+            'pagination' => $pagination,
+            'name' => Yii::$app->session->get('name')
         ]);
     }
     
     public function actionUser() 
     {
-        $name = Yii::$app->request->get("name", "Гость");
+        $name = Yii::$app->request->get("name");
+        
+        $session = Yii::$app->session;
+        $cookies = Yii::$app->response->cookies;
+        
+        $session->set('name', $name);
+        
         return $this->render('user', [
            'name' => $name 
         ]);
